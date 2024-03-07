4977ec1d784e39c4d7b358400943501e82cb953d
roccocalo <roccocalo03@gmail.com>
2024-03-07 18:23:44 +0100

    fixing images deploying error

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <>
         <div className="bg-sky-600 " >
-          <span className="flex justify-center items-center gap-1"> <img className="w-5 mt-1"  src="public/Images/github.svg" alt="gith icon" /><a href="https://github.com/roccocalo"><p className="text-white mt-1 ">Made by Rocco Calo'</p></a> </span>
+          <span className="flex justify-center items-center gap-1"> <img className="w-5 mt-1"  src="/Images/github.svg" alt="gith icon" /><a href="https://github.com/roccocalo"><p className="text-white mt-1 ">Made by Rocco Calo'</p></a> </span>
         </div>
         </>
     )
